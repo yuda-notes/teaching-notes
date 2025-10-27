64f9ec44562cac1aa5350d3a79f74ce9b2a3e3a6
Yudanto Anas <youdant@gmail.com>
2025-10-27 22:40:45 +0700

    Add files via upload

---
+++ materials/rest-api/README.md
@@ -0,0 +1,15 @@
+# REST API
+
+## Setup
+
+```shell
+pip install "fastapi[standard]"
+```
+
+## To Run the Script
+
+```shell
+fastapi dev
+# or
+fastapi dev filename.py
+```
---
+++ materials/rest-api/main.py
@@ -0,0 +1,10 @@
+# 1. Import package
+
+# 2. Create FastAPI object
+
+# 3. Create some endpoints (GET)
+'''
+3.1. endpoint main
+3.2. endpoint get all data
+3.3. endpoint get specific data (protected)
+'''
